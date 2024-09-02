d44bf0702ac70d1ace719019c3b8515de9efa1ea
Queecy <queecypowned@gmail.com>
2024-09-02 13:20:18 +0200

    Add files via upload

---
+++ Notification/Notification/Notify.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Notification
+{
+    partial class Notify
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Notify));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.msg = new System.Windows.Forms.Label();
+            this.CloseNotification = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 42);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.lblMsg.ForeColor = System.Drawing.Color.Gray;
+            this.lblMsg.Location = new System.Drawing.Point(63, 25);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(56, 14);
+            this.lblMsg.TabIndex = 53;
+            this.lblMsg.Text = "Message";
+            // 
+            // msg
+            // 
+            this.msg.AutoSize = true;
+            this.msg.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold);
+            this.msg.ForeColor = System.Drawing.Color.Cyan;
+            this.msg.Location = new System.Drawing.Point(63, 8);
+            this.msg.Name = "msg";
+            this.msg.Size = new System.Drawing.Size(91, 14);
+            this.msg.TabIndex = 54;
+            this.msg.Text = "Notification";
+            // 
+            // CloseNotification
+            // 
+            this.CloseNotification.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.CloseNotification.Cursor = System.Windows.Forms.Cursors.Default;
+            this.CloseNotification.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CloseNotification.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold);
+            this.CloseNotification.ForeColor = System.Drawing.Color.Cyan;
+            this.CloseNotification.Location = new System.Drawing.Point(295, 18);
+            this.CloseNotification.Name = "CloseNotification";
+            this.CloseNotification.Size = new System.Drawing.Size(31, 25);
+            this.CloseNotification.TabIndex = 55;
+            this.CloseNotification.Text = "X";
+            this.CloseNotification.UseVisualStyleBackColor = true;
+            this.CloseNotification.Click += new System.EventHandler(this.CloseNotification_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.Location = new System.Drawing.Point(-10, 56);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(22, 25);
+            this.panel1.TabIndex = 56;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Notify
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            this.ClientSize = new System.Drawing.Size(338, 62);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.CloseNotification);
+            this.Controls.Add(this.msg);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Notify";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Notify";
+            this.Load += new System.EventHandler(this.Notify_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Button CloseNotification;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Notification/Notification/Notify.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notification
+{
+    public partial class Notify : Form
+    {
+        private Notify.enmAction action;
+        private int x, y;
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(
+            int nLeftRect,
+            int nTopRect,
+            int nRightRect,
+            int nBottomRect,
+            int nWidthEllipse,
+            int nHeightEllipse
+        );
+
+        [DllImport("user32.dll")]
+        public static extern uint SetWindowDisplayAffinity(IntPtr hwnd, uint dwAffinity);
+
+        public enum enmAction
+        {
+            wait,
+            start,
+            close
+        }
+
+        public enum enmType
+        {
+            Success,
+            Error,
+            Warning,
+            Informaion
+            // Add more (OPTIONAL)
+        }
+
+        public Notify()
+        {
+            InitializeComponent();
+            this.Activated += new EventHandler(this.ForceTop);
+
+            this.FormBorderStyle = FormBorderStyle.None;
+            int radius = 20;
+            this.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, this.Width, this.Height, radius, radius));
+        }
+
+        private void ForceTop(object sender, EventArgs e)
+        {
+            this.TopMost = true;
+        }
+
+        private void Notify_Load(object sender, EventArgs e)
+        {
+            panel1.Width = 0;
+            FillPanelAsync(345);
+        }
+
+        private async void FillPanelAsync(int targetWidth)
+        {
+            int step = 5; 
+            while (panel1.Width < targetWidth)
+            {
+                panel1.Width += step;
+                if (panel1.Width > targetWidth)
+                {
+                    panel1.Width = targetWidth; 
+                }
+                await Task.Delay(47); 
+            }
+        }
+
+        private void CloseNotification_Click(object sender, EventArgs e)
+        {
+            action = enmAction.close;
+        }
+
+        private async void timer1_Tick(object sender, EventArgs e)
+        {
+            switch (this.action)
+            {
+                case enmAction.wait:
+                    await Task.Delay(4000);
+                    action = enmAction.close;
+                    break;
+                case Notify.enmAction.start:
+                    this.timer1.Interval = 5;
+                    this.Opacity += 0.1;
+                    if (this.x < this.Location.X)
+                    {
+                        this.Left--;
+                    }
+                    else
+                    {
+                        if (this.Opacity == 1.0)
+                        {
+                            action = Notify.enmAction.wait;
+                        }
+                    }
+                    break;
+                case enmAction.close:
+                    timer1.Interval = 5;
+                    this.Opacity -= 0.1;
+
+                    this.Left -= 3;
+                    if (base.Opacity == 0.0)
+                    {
+                        base.Close();
+                    }
+                    break;
+            }
+        }
+
+        public void showAlert(string msg, enmType type)
+        {
+            this.Opacity = 0.0;
+            this.StartPosition = FormStartPosition.Manual;
+
+            string fname;
+            for (int i = 1; i < 10; i++)
+            {
+                fname = "alert" + i.ToString();
+                Notify frm = (Notify)Application.OpenForms[fname];
+
+                if (frm == null)
+                {
+                    this.Name = fname;
+                    int padding = 5; // Break
+                    int horizontalOffset = 1450; // Horizontal offset 
+                    int verticalOffset = 5; // Vertical displacement
+
+                    this.x = Screen.PrimaryScreen.WorkingArea.Width - this.Width - horizontalOffset;
+                    this.y = verticalOffset + (this.Height + padding) * (i - 1);
+                    this.Location = new Point(this.x, this.y);
+                    break;
+                }
+            }
+            this.x = Screen.PrimaryScreen.WorkingArea.Width - base.Width - 5;
+
+            switch (type)
+            {
+                case enmType.Success:
+                    this.msg.Text = "Success";
+                    this.lblMsg.Text = msg;
+                    break;
+                case enmType.Error:
+                    this.msg.Text = "Error";
+                    this.lblMsg.Text = msg;
+                    break;
+                case enmType.Warning:
+                    this.msg.Text = "Warning";
+                    this.lblMsg.Text = msg;
+                    break;
+                case enmType.Informaion:
+                    this.msg.Text = "Information";
+                    this.lblMsg.Text = msg;
+                    break;
+            }
+
+            this.Show();
+            this.action = enmAction.start;
+            this.timer1.Interval = 5;
+            this.timer1.Start();
+        }
+    }
+}
